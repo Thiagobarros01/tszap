7011890b8a02886e4da89fa6e85730e28b5ac92f
Thiago <thiagosbarros_@hotmail.com>
2026-01-07 23:13:38 -0300

    novas correções

--- backend/zapzap/src/main/java/thiagosbarros/com/conversazap/infrastructure/controller/ApiWebhookController.java
+++ backend/zapzap/src/main/java/thiagosbarros/com/conversazap/infrastructure/controller/ApiWebhookController.java
@@ -1,11 +1,13 @@
 package thiagosbarros.com.conversazap.infrastructure.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import thiagosbarros.com.conversazap.application.usecase.ProcessarMensagemUseCase;
 import thiagosbarros.com.conversazap.interfaces.dto.MensagemEntradaDTO;
 import thiagosbarros.com.conversazap.interfaces.dto.RespostaMensagemDTO;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/webhook")
 public class ApiWebhookController {
@@ -20,10 +22,17 @@ public class ApiWebhookController {
     public ResponseEntity<RespostaMensagemDTO> receberMensagem(
             @RequestBody MensagemEntradaDTO dto
     ) {
-        System.out.println("🔌 [API] Mensagem recebida de: " + dto.telefoneCliente());
 
-        var resposta = useCase.executar(dto.toCommand());
+        log.info("🔌 [API] Recebendo mensagem de: {}", dto.telefoneCliente());
 
-        return ResponseEntity.ok(resposta);
+        try {
+            var resposta = useCase.executar(dto.toCommand());
+            log.info("✅ Mensagem processada com sucesso. Resposta: {}", resposta);
+            return ResponseEntity.ok(resposta);
+        } catch (Exception e) {
+            log.error("❌ Erro ao processar mensagem: ", e);
+            throw e;
+        }
+
     }
 }
